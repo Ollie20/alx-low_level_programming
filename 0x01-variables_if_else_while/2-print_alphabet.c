83813f7006e5ed79bb8acbb885e5db8875fd8641
John Faith <johnfayth45@gmail.com>
2023-08-11 20:59:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - origin point
+ * Description: Print lowercase alphabets
+ * Return: (0) (Success)
+ */
+int main(void)
+{
+int n = 97;
+
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
